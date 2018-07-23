649616884dddb2b28c20ae0eeb087b274ed83bfb
Unknown <anon@mail.com>
2018-07-23 16:18:00 -0400

    Update version

--- src/main/java/fluke/treetweaker/TreeTweaker.java
+++ src/main/java/fluke/treetweaker/TreeTweaker.java
@@ -24,7 +24,7 @@ public class TreeTweaker
 
 	public static final String MODID = "treetweaker";
 	public static final String NAME = "TreeTweaker";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 	public static final String DEPENDS = "required-after:crafttweaker;";
 
 	@Instance(MODID)
